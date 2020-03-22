eedb5a9286a13dd1efc15bdf5ce0b31eb5d4b30c
Leonardo Rocha <leonardoleo182@hotmail.com>
2020-03-22 01:45:17 -0300

    Projeto para mostrar a media de dois alunos

---
+++ ProjetoClasse/Nota.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoClasse
+{
+    class Nota
+    {
+        public string Name { get; set; }
+        public double Note1 { get; set; }
+        public double Note2 { get; set; }
+        public double Note3 { get; set; }
+        public double Note4 { get; set; }
+
+    }
+}
---
+++ ProjetoClasse/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProjetoClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nota student1, student2;
+
+            student1 = new Nota();
+            student2 = new Nota();
+
+            Console.WriteLine("Student data 1");
+            Console.Write("Name: ");
+            student1.Name = Console.ReadLine();
+            Console.Write("Note 1: ");
+            student1.Note1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Note 2: ");
+            student1.Note2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Note 3: ");
+            student1.Note3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Note 4: ");
+            student1.Note4 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Student data 2");
+            Console.Write("Name: ");
+            student2.Name = Console.ReadLine();
+            Console.Write("Note 1: ");
+            student2.Note1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Note 2: ");
+            student2.Note2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Note 3: ");
+            student2.Note3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Note 4: ");
+            student2.Note4 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+
+            double avg = (student1.Note1 + student1.Note2 + student1.Note3 + student1.Note4) / 4.00;
+            Console.WriteLine("Media student 1: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            avg = (student2.Note1 + student2.Note2 + student2.Note3 + student2.Note4) / 4.00;
+            Console.WriteLine("Media student 2: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
